3f5eacd89bc24cb20dcbcc1c513e8e3dbdf53cfb
athul jyothis <63841734+jyothisable@users.noreply.github.com>
2021-12-26 00:19:12 +0530

    handleSubmit done

--- 43-simple-list/before/script.js
+++ 43-simple-list/before/script.js
@@ -1,1 +1,13 @@
-console.log('hello')
+const input = document.querySelector("#item-input");
+const form = document.querySelector("#new-item-form");
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  let newItem = document.createElement("div");
+  newItem.classList.add("list-item");
+  newItem.innerText = input.value;
+  document.querySelector("#list").appendChild(newItem);
+  input.value = "";
+};
+
+form.onsubmit = handleSubmit;
