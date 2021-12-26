25f38b5a91b0085cb85a73389dc269cdc9834b0b
athul jyothis <63841734+jyothisable@users.noreply.github.com>
2021-12-27 02:14:58 +0530

    done

--- 54-55-quiz/before/script.js
+++ 54-55-quiz/before/script.js
@@ -1,16 +1,31 @@
-/*
-  TODO: 2. Select all elements needed
-    * The form element (has the id `quiz-form`)
-    * The answer inputs (have the class `answer`)
-    * BONUS: The questions (have the class `question-item`)
-    * BONUS: The alert (has the id `alert`)
-*/
+const form = document.getElementById("quiz-form");
+const questionItems = document.querySelectorAll(".question-item");
+const alert = document.querySelector("#alert");
 
-// TODO: 3. Create a submit event listener for the form that does the following.
-//    1. Prevent the default behaviour
-//    2. Get all selected answers (use the `checked` property on the input to determine if it is selected or not)
-//    3. Loop through the selected answer to see if they are correct or not (Check the value of the answer to see if it is the string "true")
-//    4. For each correct answer add the class `correct` to the parent with the class `question-item` and remove the class `incorrect`.
-//    5. For each incorrect answer add the class `incorrect` to the parent with the class `question-item` and remove the class `correct`.
-//    6. BONUS: Make sure unanswered questions show up as incorrect. The easiest way to do this is to add the incorrect class and removing the correct class from all question items before checking the correct answers
-//    7. BONUS: If all answers are correct show the element with the id `alert` and hide it after one second (look into setTimeout) (use the class active to show the alert and remove the class to hide it)
+const handleSubmit = (e) => {
+  e.preventDefault();
+  correctCnt = 0;
+  questionItems.forEach((question) => {
+    answerOptions = Array.from(question.querySelectorAll(".answer"));
+    console.log(answerOptions);
+    correctAns = answerOptions.filter((option) => {
+      return option.value === "true" && option.checked;
+    });
+    if (correctAns.length) {
+      question.classList.add("correct");
+      question.classList.remove("incorrect");
+      correctCnt++;
+    }
+    else {
+      question.classList.add("incorrect");
+    }
+  });
+  if (correctCnt === questionItems.length) {
+    alert.classList.add("active");
+    setTimeout(() => {
+      alert.classList.remove("active");
+    }, 1000);
+  }
+};
+
+form.onsubmit = handleSubmit;
